a8e5325289ab6a89f4eea2a45b24e139fcf59f8f
97252 <maorroqm14@gmail.com>
2021-01-15 10:37:02 +0200

    deploying to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/weather'));
+app.use(express.static(__dirname + '/dist/Weather/'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/weather/index.html'));});
